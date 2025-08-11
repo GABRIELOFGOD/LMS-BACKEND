75af9e8e487294904a8fed5f2d4ff55c1f3e01a4
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-08-11 23:16:06 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME!

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,15 +21,24 @@ import jwtConfig from './config/jwt.config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
+      // useFactory: (configService: ConfigService) => ({
+      //   type: 'postgres',
+      //   host: configService.get<string>('DATABASE_HOST'),
+      //   port: configService.get<number>('DATABASE_PORT'),
+      //   username: configService.get<string>('DATABASE_USER'),
+      //   password: configService.get<string>('DATABASE_PASSWORD'),
+      //   database: configService.get<string>('DATABASE_NAME'),
+      //   entities: [Course, Chapters, Attachment, User],
+      //   synchronize: true, // ⚠️ Set to false in production!
+      // }),
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
-        port: configService.get<number>('DATABASE_PORT'),
-        username: configService.get<string>('DATABASE_USER'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_NAME'),
+        url: configService.get<string>('DATABASE_URL'),
         entities: [Course, Chapters, Attachment, User],
         synchronize: true, // ⚠️ Set to false in production!
+        ssl: process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false
       }),
     }),
     ChaptersModule,
